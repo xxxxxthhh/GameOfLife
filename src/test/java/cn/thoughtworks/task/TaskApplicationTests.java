b83f869c1037523575d17a05257fdb7943d3f081
yc <yc95227@gmail.com>
2019-06-12 12:59:57 +0800

    init

---
+++ src/test/java/cn/thoughtworks/task/TaskApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.thoughtworks.task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class TaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("test");
+	}
+
+}
